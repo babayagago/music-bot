f7add0875c0b6721a3045a2cc85b073c374f7c67
babayagago <oboussardmickael@gmail.com>
2026-04-11 01:06:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
+:::writing{variant=“standard” id=“10002”}
+const { Client, GatewayIntentBits, Events } = require(‘discord.js’);
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require(’@discordjs/voice’);
+const play = require(‘play-dl’);
+
+const client = new Client({
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildVoiceStates
+]
+});
+
+const queues = new Map();
+
+client.once(Events.ClientReady, () => {
+console.log(Connecté en tant que ${client.user.tag});
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+if (!interaction.isChatInputCommand()) return;
+
+const guildId = interaction.guildId;
+
+if (!queues.has(guildId)) {
+queues.set(guildId, []);
+}
+
+const queue = queues.get(guildId);
+
+if (interaction.commandName === ‘play’) {
+const url = interaction.options.getString(‘url’);
+const voiceChannel = interaction.member.voice.channel;
+  if (!voiceChannel) {
+  return interaction.reply({ content: "Rejoins un vocal", ephemeral: true });
+}
+
+queue.push(url);
+await interaction.reply("Ajouté à la queue");
+
+if (queue.length === 1) {
+  playMusic(interaction, voiceChannel);
+  }
+
+if (interaction.commandName === ‘skip’) {
+queue.shift();
+playMusic(interaction, interaction.member.voice.channel);
+interaction.reply(“Skip”);
+}
+
+if (interaction.commandName === ‘stop’) {
+queues.set(guildId, []);
+interaction.reply(“Stop”);
+}
+});
+
+async function playMusic(interaction, voiceChannel) {
+const queue = queues.get(interaction.guildId);
+if (!queue || queue.length === 0) return;
+
+const url = queue[0];
+
+const connection = joinVoiceChannel({
+channelId: voiceChannel.id,
+guildId: interaction.guildId,
+adapterCreator: interaction.guild.voiceAdapterCreator
+});
+
+const stream = await play.stream(url);
+const resource = createAudioResource(stream.stream, {
+inputType: stream.type
+});
+
+const player = createAudioPlayer();
+player.play(resource);
+connection.subscribe(player);
+
+interaction.channel.send(Lecture: ${url});
+
+player.on(AudioPlayerStatus.Idle, () => {
+queue.shift();
+playMusic(interaction, voiceChannel);
+});
+}
+
+client.login(process.env.TOKEN);
+  
+}
 
